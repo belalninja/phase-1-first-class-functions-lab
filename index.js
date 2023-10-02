ec532ef6550dd1a31d3f8ae4d6bdb99ee93aefbe
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-10-02 07:49:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array) {
+  const newArray = [...array.slice(0, 2)];
+  return newArray;
+};
+
+const returnLastTwoDrivers = function (array) {
+  const newArray = [...array.slice(-2)];
+  return newArray;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+  return function (num) {
+    return fare * num;
+  };
+}
+
+const fareDoubler = function (fare) {
+  return createFareMultiplier(fare)(2);
+};
+
+const fareTripler = function (fare) {
+  return createFareMultiplier(fare)(3);
+};
+
+function selectDifferentDrivers(array, fun) {
+  return fun(array);
+}
